8d147efdce53a64ae422e2789331cc5212edacd4
Смирнов Михаил Владимирович <mi.v.smirnov@tinkoff.ru>
2024-10-26 17:11:59 +0300

    from person1

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from file_operations import read_file, write_file
+from file_operations import read_file, write_file, delete_last_lines
 from utils import get_user_input
 
 def main():
     filename = 'example.txt'
 
     while True:
-        action = get_user_input("Введите 'r' для чтения файла, 'w' для записи, 'q' для выхода: ")
+        action = get_user_input("Введите 'r' для чтения файла, 'w' для записи, 'd' для удаления последних строк, 'q' для выхода: ")
 
         if action == 'r':
             content = read_file(filename)
@@ -15,6 +15,10 @@ def main():
             text = input("Введите текст для записи в файл: ")
             write_file(filename, text)
             print("Текст записан в файл.")
+        elif action == 'd':
+            num_lines = int(input("Сколько последних строк удалить? "))
+            delete_last_lines(filename, num_lines)
+            print(f"{num_lines} последняя(ие) строка(и) удалена(ы) из файла.")
         elif action == 'q':
             print("Выход из программы.")
             break
