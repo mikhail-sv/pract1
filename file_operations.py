ac0784c046e09720730562617d4b2e4c4899d498
Смирнов Михаил Владимирович <mi.v.smirnov@tinkoff.ru>
2024-10-26 17:04:56 +0300

    from person2

--- file_operations.py
+++ file_operations.py
@@ -10,3 +10,17 @@ def write_file(filename, text):
     """Запись текста в текстовый файл."""
     with open(filename, 'a', encoding='utf-8') as file:
         file.write(text + '\n')
+
+def delete_last_lines(filename, num_lines=1):
+    """Удаление последних строк из текстового файла."""
+    try:
+        with open(filename, 'r', encoding='utf-8') as file:
+            lines = file.readlines()
+
+        # Удаляем последние строки
+        new_lines = lines[:-num_lines]
+
+        with open(filename, 'w', encoding='utf-8') as file:
+            file.writelines(new_lines)
+    except FileNotFoundError:
+        return "Файл не найден."
